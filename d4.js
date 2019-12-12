87b18651f9e21d4225ab4090067be1c5beb15cdb
Robert Paul <rob.k.paul@gmail.com>
2019-12-11 20:01:20 -0800

    Day 4 Done!

--- d4.js
+++ d4.js
@@ -1,38 +1,76 @@
 function searchDigits(start, end) {
   let matches = 0;
   for (let num = start; num <= end; num++) {
-      let current = num.toString();
-      }
-      if(hasDouble(current) && double){
-        matches++;
-      }
-      prev = current;
+    let current = num.toString();
+    if(doesIncrease(current, 0) && hasDouble(current, 0)) {
+      matches++;
     }
-    }
+  }
   return matches;
 }
-function doesIncrease(number, prev){
-  if(number.length === 0){
-    return true;
+
+function searchDigitsUpdated(start, end) {
+  let matches = 0;
+  for (let num = start; num <= end; num++) {
+    let current = num.toString();
+    if(doesIncrease(current) && hasDoubleNoRepeat(current)) {
+      matches++;
+    }
   }
-  let current = parseInt(number[0]);
-  if(current < prev){
-    return doesIncrease(number[1], current);
-  }
-  else{
+  return matches;
+}
+
+function doesIncrease(num){
+  for (var i = 0; i < num.length; i++) {
+    let current = parseInt(num[i]);
+    let prev = null;
+    let prevprev = null;
+    if(i>0){
+      prev = parseInt(num[i-1]);
+    }
+    if(prev > current){
       return false;
+    }
+    else if(i===5){
+      return true;
+    }
   }
 }
-function hasDouble(number, prev) {
-    if(number.length === 0){
-        return false;
+
+function hasDouble(num) {
+  for (var i = 0; i < num.length; i++) {
+    let current = parseInt(num[i]);
+    let prev = null;
+    if(i>0){
+      prev = parseInt(num[i-1]);
     }
-    let current = parseInt(number.toString()[0]);
-    if(number === prev){
-        return true;
+    if(prev === current){
+      return true;
     }
-    else{
-        return hasDouble(number[1], current);
-    }
+  }
+  return false;
 }
-console.log(searchDigits(171309, 643603));
+
+function hasDoubleNoRepeat(num) {
+  for (var i = 0; i < num.length; i++) {
+    let current = parseInt(num[i]);
+    let prev = null;
+    let prev2 = null;
+    let next = parseInt(num[i+1]);
+    if(i>0){
+      if(i>1){
+          prev2 = parseInt(num[i-2]);
+      }
+      prev = parseInt(num[i-1]);
+    }
+    if(prev === current
+    && prev2 != current
+    && next != current) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(`Day 4 Pt. 2: ${searchDigitsUpdated(171309, 643603)}`);
+console.log('Day 4: ' + searchDigits(171309, 643603));
